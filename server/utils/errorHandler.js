fc028387666c549f6bf9025cbcb755d2ffec2cc9
amajala chakravarthi <chakrinaiduamajala@gmail.com>
2025-11-10 07:54:15 +0530

    Update errorHandler.js

--- server/utils/errorHandler.js
+++ server/utils/errorHandler.js
@@ -1,10 +1,11 @@
 class ErrorHandler extends Error {
-    constructor(message, statusCode) {
-        super(message);
-        this.statusCode = statusCode;
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
 
-        Error.captureStackTrace(this, this.constructor);
-    }
+    // Capture stack trace for better debugging
+    Error.captureStackTrace(this, this.constructor);
+  }
 }
 
 module.exports = ErrorHandler;
